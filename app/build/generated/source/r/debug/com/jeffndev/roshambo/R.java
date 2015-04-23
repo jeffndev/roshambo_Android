3ff9b3fdc1b758549f794a0d22c420d59e8eadd1
unknown <jnewel200@PAWCHL-GCWDF12.cable.comcast.com>
2015-04-23 10:34:40 -0400

    fully implemented and tested game play

--- app/build/generated/source/r/debug/com/jeffndev/roshambo/R.java
+++ app/build/generated/source/r/debug/com/jeffndev/roshambo/R.java
@@ -1440,6 +1440,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0b0010;
         public static final int app_name=0x7f0b0011;
         public static final int hello_world=0x7f0b0012;
+        public static final int status_text_tie=0x7f0b0013;
     }
     public static final class style {
         /**  Customize your theme here. 
--- app/src/main/java/com/jeffndev/roshambo/RoshamboResultFragment.java
+++ app/src/main/java/com/jeffndev/roshambo/RoshamboResultFragment.java
@@ -3,6 +3,7 @@ package com.jeffndev.roshambo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
  * Created by jeffreynewell1 on 4/22/15.
  */
 public class RoshamboResultFragment extends Fragment{
+    private final String LOG_TAG = RoshamboResultFragment.class.getSimpleName();
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         int rps = RoshamboResultActivity.ROCK;
@@ -30,9 +34,33 @@ public class RoshamboResultFragment extends Fragment{
         ImageView imgStatus = (ImageView)rootView.findViewById(R.id.frag_result_status_imageview);
         TextView txtStatus = (TextView)rootView.findViewById(R.id.frag_result_status_textview);
 
-        imgStatus.setImageResource(R.drawable.rock_crushes_scissors); //TODO: has to depend on choices..
-        txtStatus.setText("Rock crushes scissors, I win!");//TODO: put these strings as format strings in the string resources..
+        setGameStatusVisuals(rps, dealerRps, txtStatus, imgStatus);
 
         return rootView;
+    }
+    private void setGameStatusVisuals(int playerRps, int dealerRps,
+                                      TextView statusLbl, ImageView statusImg){
+        Log.v(LOG_TAG,"player: " + playerRps + "\tdealer: " + dealerRps);
+        //TODO: put these strings as format strings in the string resources..
+        if(playerRps == dealerRps) {
+            statusLbl.setText(getString(R.string.status_text_tie));
+            statusImg.setImageResource(R.drawable.its_a_tie);
+            return;
+        }
+        int[] rpsChoices = {playerRps, dealerRps};
+        Arrays.sort(rpsChoices);
+        if(rpsChoices[0] == RoshamboResultActivity.ROCK && rpsChoices[1] == RoshamboResultActivity.PAPER){
+            //rock/paper
+            statusLbl.setText("Paper covers rock, " + (playerRps == RoshamboResultActivity.PAPER ? "you" : "I" ) + " win!");
+            statusImg.setImageResource(R.drawable.paper_covers_rock);
+        }else if(rpsChoices[0] == RoshamboResultActivity.ROCK){
+            //rock/scissors
+            statusLbl.setText("Rock crushes scissors, " + (playerRps == RoshamboResultActivity.ROCK ? "you" : "I" ) + " win!");
+            statusImg.setImageResource(R.drawable.rock_crushes_scissors);
+        }else{
+            //paper/scissors
+            statusLbl.setText("Scissors cut paper, " + (playerRps == RoshamboResultActivity.SCISSORS ? "you" : "I" ) + " win!");
+            statusImg.setImageResource(R.drawable.scissors_cut_paper);
+        }
     }
 }
